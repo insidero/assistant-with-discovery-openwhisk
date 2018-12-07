18984db6c8c5214542467e67358f3c20b4430f38
Hamza Rashid Bodla <hamzabodla@gmail.com>
2018-12-07 17:00:18 +0500

    button add

--- src/Conversation.js
+++ src/Conversation.js
@@ -27,14 +27,10 @@ function Conversation(props) {
       </div>
       <div className="conversation__input-container">
         <InputWithButton className="conversation__input" onSubmit={props.onSubmit} placeholder="Say something to Watson."/>
+        <Button >hi</Button>
       </div>
       <div className="conversation__disclaimer--message">
-        <h6 className="base--h6" >
-          * This system is for demonstration purposes only and is not intended to process
-          Personal Data. No Personal Data is to be entered into this system as it may not
-          have the necessary controls in place to meet the requirements of the General Data
-          Protection Regulation (EU) 2016/679.
-        </h6>
+        
       </div>
     </div>
   );
