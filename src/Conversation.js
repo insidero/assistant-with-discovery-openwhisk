4e476ea8e878b9f72a695d93620231bf9d539fbd
Hamza Rashid Bodla <hamzabodla@gmail.com>
2018-12-10 11:35:47 +0500

    asd

--- src/Conversation.js
+++ src/Conversation.js
@@ -34,7 +34,7 @@ function Conversation(props) {
 
          <AudioRecorder 
          downloadable: true
-         filename: audio.wav
+      
          onChange={props.onChange}
          />
       </div>
